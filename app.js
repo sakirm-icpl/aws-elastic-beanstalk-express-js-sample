f453dd33351e9b11f01da8878226b1e887a028b3
sakir <sakirm@infopercept.com>
2023-11-23 15:10:29 +0530

    new

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to Infopercept!'));
+app.get('/', (req, res) => res.send('welocme to my web page'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
